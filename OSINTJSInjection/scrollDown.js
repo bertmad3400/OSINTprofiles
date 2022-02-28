bd21a80b3ca7a3ad9fc35b27b438e2974adda0c8
bertmad3400 <skrivtilbertram@gmail.com>
2022-02-28 10:42:48 +0100

    Add more delay to scroll down js injection

--- OSINTJSInjection/scrollDown.js
+++ OSINTJSInjection/scrollDown.js
@@ -1,17 +1,22 @@
 document.osinterReady = false;
 
+console.log(document.documentElement.clientHeight)
+
 function scroll(lastOffset, i) {
 	window.scrollTo(lastOffset, lastOffset + document.documentElement.clientHeight);
-	var newOffset = window.pageYOffset;
 
-	i = i + 1;
+	setTimeout(function() {
+		var newOffset = window.pageYOffset;
 
-	if (i <= 10 && newOffset != lastOffset) {
-		setTimeout(scroll, 1000, newOffset, i);
-	} else {
-		document.osinterReady = true;
-		console.log(newOffset, lastOffset)
-	}
+		i = i + 1;
+
+		if (i <= 10 && newOffset != lastOffset) {
+			setTimeout(scroll, 1000, newOffset, i);
+		} else {
+			document.osinterReady = true;
+			console.log(newOffset, lastOffset)
+		}
+	}, 500);
 }
 
 scroll(0, 0);
